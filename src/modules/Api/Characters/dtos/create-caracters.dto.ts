5aec65b07d28b561dc54176b56d1b15a5471eee5
SantiRosso <santiagorosso@live.com.ar>
2023-08-12 15:04:32 -0300

    class-validator. Character validations

--- src/modules/Api/Characters/dtos/create-caracters.dto.ts
+++ src/modules/Api/Characters/dtos/create-caracters.dto.ts
@@ -1,6 +1,19 @@
+import { IsString, IsNotEmpty, IsNumber, IsBoolean } from 'class-validator';
+
 export class createCharacterDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsString()
   house: string;
+
+  @IsNotEmpty()
+  @IsNumber()
   age: number;
+
+  @IsNotEmpty()
+  @IsBoolean()
   alive: boolean;
 }
