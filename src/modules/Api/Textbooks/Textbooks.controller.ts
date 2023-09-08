0c99fab155b14d96094b9fea82078f5c82077bfd
SantiRosso <santiagorosso@live.com.ar>
2023-09-08 11:15:38 -0300

    Textbook, GET and GET(author)

--- src/modules/Api/Textbooks/Textbooks.controller.ts
+++ src/modules/Api/Textbooks/Textbooks.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { TextbookService } from './Textbooks.service';
 
 @Controller('api/textbooks')
 export class TextbookController {
-  constructor(private textbooksService: TextbookService) {}
+  constructor(private textbookService: TextbookService) {}
+
+  @Get()
+  async findAll(@Query('author') author: string) {
+    const textbooks = author
+      ? await this.textbookService.findByAuthor(author)
+      : await this.textbookService.findAll();
+    return textbooks;
+  }
 }
--- src/modules/Api/Textbooks/Textbooks.service.ts
+++ src/modules/Api/Textbooks/Textbooks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Textbook } from './entities/Textbooks.entity';
 import { Repository } from 'typeorm';
@@ -9,4 +9,22 @@ export class TextbookService {
     @InjectRepository(Textbook)
     private textbookRepository: Repository<Textbook>,
   ) {}
+
+  async findAll() {
+    return this.textbookRepository.find();
+  }
+
+  async findByAuthor(author: string) {
+    const textbookFound = await this.textbookRepository.find({
+      where: {
+        author,
+      },
+    });
+
+    if (!textbookFound) {
+      return new HttpException('Character not found', HttpStatus.NOT_FOUND);
+    }
+
+    return textbookFound;
+  }
 }
