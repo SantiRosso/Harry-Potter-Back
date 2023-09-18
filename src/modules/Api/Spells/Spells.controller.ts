d149dff4f2688a25ed1996d2989bdc7a9e2742f4
SantiRosso <santiagorosso@live.com.ar>
2023-09-18 20:50:19 -0300

    Spells Controller

--- src/modules/Api/Spells/Spells.controller.ts
+++ src/modules/Api/Spells/Spells.controller.ts
@@ -1,7 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { SpellService } from './Spells.service';
+import { CreateSpellDto } from './dtos/create-spells.dto';
+import { UpdateSpellDto } from './dtos/update-spells.dto';
 
 @Controller('api/spells')
 export class SpellController {
   constructor(private spellService: SpellService) {}
+
+  @Get()
+  async findAll() {
+    return await this.spellService.findAll();
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string) {
+    return await this.spellService.findById(id);
+  }
+
+  @Post()
+  async create(@Body() createFields: CreateSpellDto) {
+    return await this.spellService.create(createFields);
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() updateFields: UpdateSpellDto) {
+    return await this.spellService.update(id, updateFields);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return await this.spellService.delete(id);
+  }
 }
