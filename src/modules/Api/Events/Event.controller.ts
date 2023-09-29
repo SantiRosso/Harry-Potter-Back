c880acbcad9ec502c5418456567df48b6cd48b61
SantiRosso <santiagorosso@live.com.ar>
2023-09-29 13:15:17 -0300

    EventController

--- src/modules/Api/Events/Event.controller.ts
+++ src/modules/Api/Events/Event.controller.ts
@@ -1,7 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Delete,
+  Patch,
+} from '@nestjs/common';
 import { EventService } from './Event.service';
+import { CreateEventDto } from './dtos/create-event.dto';
+import { UpdateEventDto } from './dtos/update-event.dto';
 
 @Controller('api/events')
 export class EventController {
   constructor(private eventService: EventService) {}
+
+  @Get()
+  async findAll() {
+    return await this.eventService.findAll();
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string) {
+    return await this.eventService.findById(id);
+  }
+
+  @Post()
+  async create(@Body() createFields: CreateEventDto) {
+    return this.eventService.create(createFields);
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() updateFields: UpdateEventDto) {
+    return this.eventService.update(id, updateFields);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.eventService.delete(id);
+  }
 }
