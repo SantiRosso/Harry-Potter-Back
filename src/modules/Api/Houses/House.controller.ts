02d2ba5ab2cf8bf76dd6a6971e9617275f2b5863
SantiRosso <santiagorosso@live.com.ar>
2023-10-01 18:57:57 -0300

    HouseController

--- src/modules/Api/Houses/House.controller.ts
+++ src/modules/Api/Houses/House.controller.ts
@@ -1,7 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Patch,
+  Body,
+  Param,
+} from '@nestjs/common';
 import { HouseService } from './House.service';
+import { CreateHouseDto } from './dtos/CreateHouse.dto';
+import { UpdateHouseDto } from './dtos/UpdateHouse.dto';
 
 @Controller('api/houses')
 export class HouseController {
   constructor(private houseService: HouseService) {}
+
+  @Get()
+  async findAll() {
+    return await this.houseService.findAll();
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string) {
+    return await this.houseService.findById(id);
+  }
+
+  @Post()
+  async create(@Body() createFields: CreateHouseDto) {
+    return await this.houseService.create(createFields);
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() updateFields: UpdateHouseDto) {
+    return await this.houseService.update(id, updateFields);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return await this.houseService.delete(id);
+  }
 }
