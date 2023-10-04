f4bbe0e9b75e72501767fdb050ce5eafe04b090c
SantiRosso <santiagorosso@live.com.ar>
2023-10-03 22:59:44 -0300

    FoodController

--- src/modules/Api/Food and drinks/Food.controller.ts
+++ src/modules/Api/Food and drinks/Food.controller.ts
@@ -1,7 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Patch,
+  Body,
+  Param,
+} from '@nestjs/common';
 import { FoodService } from './Food.service';
+import { UpdateFoodDto } from './dtos/UpdateFood.dto';
+import { CreateFoodDto } from './dtos/CreateFood.dto';
 
 @Controller()
 export class FoodController {
   constructor(private foodService: FoodService) {}
+
+  @Get()
+  async findAll() {
+    return await this.foodService.findAll();
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string) {
+    return await this.foodService.findById(id);
+  }
+
+  @Post()
+  async create(@Body() createDto: CreateFoodDto) {
+    return await this.foodService.create(createDto);
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() updateFields: UpdateFoodDto) {
+    return await this.foodService.update(id, updateFields);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return await this.foodService.delete(id);
+  }
 }
